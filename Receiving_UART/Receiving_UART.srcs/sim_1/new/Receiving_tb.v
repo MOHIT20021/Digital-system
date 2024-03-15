88bc256f00b86fbb7c0bdac451de65662d1014c1
MOHIT20021 <mohit.maurya@iitgn.ac.in>
2024-03-15 06:54:56 +0530

    RECEIVER AND TRASNMITTER WORKING MODULE

---
+++ Receiving_UART/Receiving_UART.srcs/sim_1/new/Receiving_tb.v
@@ -0,0 +1,109 @@
+`timescale 1ns / 10ps
+
+module tb_Receiving;
+reg slow_clk=0;
+reg clk=0;
+reg Rx_data=1;
+reg RESET;
+wire [7:0] Data;
+wire is_full;
+Receiving UUT (
+    .clk(slow_clk),
+    .Rx_data(Rx_data),.reset(RESET),
+    .Data(Data),
+    .is_full(is_full)
+);
+
+always #10 clk=~clk;
+always #0.1 slow_clk =~ slow_clk;
+initial 
+begin
+    #10;
+    RESET=0;
+    // Test case 1: Sending a byte 10101101
+    Rx_data = 0; // Start bit
+    #10;
+    Rx_data = 1; // Data bit 0
+    #10;
+    Rx_data = 0; // Data bit 1
+    #10;
+    Rx_data = 1; // Data bit 2
+    #10;
+    Rx_data = 1; // Data bit 3
+    #10;
+    Rx_data = 0; // Data bit 4
+    #10;
+    Rx_data = 1; // Data bit 5
+    #10;
+    Rx_data = 0; // Data bit 6
+    #10;
+    Rx_data = 1; // Data bit 7
+    #10;
+    Rx_data = 1; // Stop bit
+    #50;
+    RESET=1;#2;
+    Rx_data=0;#1;
+
+    Rx_data=1;#5;
+    RESET=0;
+    
+    // Test case 2: Sending a byte 11011001
+    Rx_data = 0; // Start bit
+    #10;
+    Rx_data = 1; // Data bit 0
+    #10;
+    Rx_data = 0; // Data bit 1
+    #10;
+    Rx_data = 0; // Data bit 2
+    #10;
+    Rx_data = 1; // Data bit 3
+    #10;
+    Rx_data = 1; // Data bit 4
+    #10;
+    Rx_data = 0; // Data bit 5
+    #10;
+    Rx_data = 1; // Data bit 6
+    #10;
+    Rx_data = 1; // Data bit 7
+    #10;
+    Rx_data = 1; // Stop bit
+    #50;
+    
+    
+    RESET=1;#2;
+    Rx_data=0;#1;
+ 
+    // Test case 1: Sending a byte 10101101
+    Rx_data = 0; // Start bit
+    #10;
+    Rx_data = 1; // Data bit 0
+    #10;
+    Rx_data = 0; // Data bit 1
+    #10;
+    Rx_data = 1; // Data bit 2
+    #10;
+    Rx_data = 1; // Data bit 3
+    #10;
+    Rx_data = 0; // Data bit 4
+    #10;
+    Rx_data = 1; // Data bit 5
+    #10;
+    Rx_data = 0; // Data bit 6
+    #10;
+    Rx_data = 1; // Data bit 7
+    #10;
+    Rx_data = 1; // Stop bit
+    #50;
+    
+    
+
+    // Display the output
+    $display("Data: %h", Data);
+
+    // Test case 2: No data received
+    Rx_data = 1; // Idle state
+    #20
+    $finish;
+end
+
+endmodule
---
+++ TRANSMITTER/TRANSMITTER.srcs/sim_1/new/Tx_tb.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 10ps
+
+
+//module Transmitter (
+//    input clk,             // System clock
+//    input reset,         // Reset signal (active low)
+//    input [7:0] data_in,   // Data to be transmitted
+//    input send,
+//    output reg tx_out,
+//    output reg is_complete=0   // UART transmit output
+//);
+module Tx_tb(
+);
+reg slow_clk=0;
+reg clk=0;
+
+reg reset,send;
+
+reg [7:0] data_in;
+
+wire tx_out;
+wire is_complete;
+
+
+
+Transmitter uut(.clk(slow_clk),.reset(reset),.data_in(data_in),.send(send),.tx_out(tx_out),.is_complete(is_complete));
+
+always #10 clk = ~clk;
+always #1 slow_clk = ~slow_clk;
+
+
+initial
+begin 
+
+reset=1; data_in=8'b10101010;send=0;#5;
+
+reset=0; data_in=8'b10101010;send=0;#2;
+reset=0; data_in=8'b10101010;send=1;#120;
+//reset=0; data_in=8'b10101010;send=1;#100;
+
+//reset=0; data_in=8'b10101010;send=0;#50;
+reset=1; data_in=8'b10110110;send=0;#5;
+
+reset=0; data_in=8'b10110110;send=0;#2;
+reset=0; data_in=8'b10110110;send=1;#120;
+
+$finish();
+end
+endmodule
